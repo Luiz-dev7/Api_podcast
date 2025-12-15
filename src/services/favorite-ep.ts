85222ce8906fd42429b5038554f7a5180710c8bd
Luiz Guilherme De Oliveira Alves <luizinlives@gmail.com>
2025-12-15 17:26:06 -0300

    adicionando episodios, e criando o arquivo de favorite

---
+++ src/services/favorite-ep.ts
@@ -0,0 +1,19 @@
+import { StatusCode } from "../utils/status-code";
+import {FilterModel} from "../models/filter-model"
+import { repoPodcast } from "../repositories/podcast-repositore"
+
+
+    export const favoriteEpisode = () => {
+    let responseFormat:FilterModel= {
+        StatusCode : 0 ,
+        Body: [],
+    };
+
+
+    
+
+
+}
+
+
+
